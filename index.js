01350124485962ec910accc2dfee08174b422e1e
Mani1211 <smanivasagamtnj@gmail.com>
2026-02-13 11:14:33 +0530

    added booking status also

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ export default async ({ req, res, log, error }) => {
   const databases = new Databases(client);
 
   const databaseId = process.env.VITE_APPWRITE_DATABASE_ID;
-  const collectionId = process.env.VITE_APPWRITE_REQUEST_ID;
+  const requestCollectionId = process.env.VITE_APPWRITE_REQUEST_ID;
+  const bookingCollectionId = process.env.VITE_APPWRITE_BOOKING_COLLECTION_ID;
 
   try {
     const today = new Date();
@@ -19,29 +20,81 @@ export default async ({ req, res, log, error }) => {
 
     let offset = 0;
     const limit = 50;
-    let updatedCount = 0;
+    let requestUpdatedCount = 0;
+    let bookingUpdatedCount = 0;
+    let bookingTravellingCount = 0;
 
     while (true) {
-      const response = await databases.listDocuments(databaseId, collectionId, [
-        Query.lessThan("returnDate", today.toISOString()),
+      const response = await databases.listDocuments(databaseId, requestCollectionId, [
+        Query.lessThanEqual("returnDate", today.toISOString()),
         Query.equal("status", "Booking Confirmed"),
         Query.limit(limit),
         Query.offset(offset),
         Query.orderAsc("$createdAt")
       ]);
 
-      console.log('response', response.total, response.documents.length)
+      console.log('request response', response.total, response.documents.length)
 
-      if (response.documents.length === 0 || updatedCount >= response.total) break;
+      if (response.documents.length === 0 || requestUpdatedCount >= response.total) break;
       // if (response.documents.length === 0 || updatedCount === 10) break;
 
       for (const doc of response.documents) {
-        await databases.updateDocument(databaseId, collectionId, doc.$id, {
+        await databases.updateDocument(databaseId, requestCollectionId, doc.$id, {
           status: "Adventure Completed",
         });
 
         updatedCount++;
         console.log(`✅ Updated request ${doc.$id} to Adventure Completed`);
+      }
+
+      offset += limit;
+    }
+
+     while (true) {
+      const response = await databases.listDocuments(databaseId, bookingCollectionId, [
+        Query.lessThanEqual("returnDate", today.toISOString()),
+        Query.limit(limit),
+        Query.offset(offset),
+        Query.orderAsc("$createdAt")
+      ]);
+
+      console.log('booking response', response.total, response.documents.length)
+
+      if (response.documents.length === 0 || bookingUpdatedCount >= response.total) break;
+      // if (response.documents.length === 0 || updatedCount === 10) break;
+
+      for (const doc of response.documents) {
+        await databases.updateDocument(databaseId, bookingCollectionId, doc.$id, {
+          status: "Travel completed",
+        });
+
+        bookingUpdatedCount++;
+        console.log(`✅ Updated booking ${doc.$id} to Travel completed`);
+      }
+
+      offset += limit;
+    }
+     while (true) {
+      const response = await databases.listDocuments(databaseId, bookingCollectionId, [
+        Query.greaterThanEqual('onwardDate', today.toISOString()),
+        Query.lessThan("returnDate", today.toISOString()),
+        Query.limit(limit),
+        Query.offset(offset),
+        Query.orderAsc("$createdAt")
+      ]);
+
+      console.log('booking response', response.total, response.documents.length)
+
+      if (response.documents.length === 0 || bookingUpdatedCount >= response.total) break;
+      // if (response.documents.length === 0 || updatedCount === 10) break;
+
+      for (const doc of response.documents) {
+        await databases.updateDocument(databaseId, bookingCollectionId, doc.$id, {
+          status: "Yet to travel",
+        });
+
+        bookingUpdatedCount++;
+        console.log(`✅ Updated booking ${doc.$id} to Yet to travel`);
       }
 
       offset += limit;
@@ -61,67 +114,3 @@ export default async ({ req, res, log, error }) => {
     };
   }
 };
-
-
-// const automate = async ()=>{
-//    const client = new Client()
-//     .setEndpoint(process.env.VITE_APPWRITE_URL)
-//     .setProject(process.env.VITE_APPWRITE_PROJECT_ID)
-//     .setKey(process.env.APPWRITE_API_KEY);
-
-//   const databases = new Databases(client);
-
-//   const databaseId = process.env.VITE_APPWRITE_DATABASE_ID;
-//   const collectionId = process.env.VITE_APPWRITE_REQUEST_ID;
-
-//   try {
-//     const today = new Date();
-//     today.setHours(0, 0, 0, 0); // start of today
-
-//     let offset = 0;
-//     const limit = 30;
-//     let updatedCount = 0;
-
-//     while (true) {
-//       const response = await databases.listDocuments(databaseId, collectionId, [
-//         Query.lessThan("returnDate", today.toISOString()),
-//         Query.equal("status", "Booking Confirmed"),
-//         Query.limit(limit),
-//         Query.offset(offset),
-//         Query.orderAsc("$createdAt")
-//       ]);
-
-//       console.log('response', response.total, response.documents.length)
-
-//       if (response.documents.length === 0 || updatedCount >= response.total) break;
-
-//       for (const doc of response.documents) {
-//         await databases.updateDocument(databaseId, collectionId, doc.$id, {
-//           status: "Adventure Completed",
-//         });
-
-//         updatedCount++;
-//         console.log(`✅ Updated request ${doc.$id} to Adventure Completed`);
-//       }
-
-//       offset += limit;
-//     }
-
-//     return {
-//       success: true,
-//       message: "Daily request status update completed",
-//       updatedCount,
-//     };
-//   } catch (err) {
-//     console.error("❌ Error updating request statuses: " + err.message);
-
-//     return {
-//       success: false,
-//       error: err.message,
-//     };
-//   }
-// }
-
-
-// const res = automate()
-// console.log('res', res)
